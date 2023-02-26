9913cb63a0e914aff3d04020ee024af56f12ade5
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:14:01 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-# module "vpc" {
-#   source = "git::https://github.com/b52-clouddevops/tf-module-vpc.git"
-# }
-
-# By default or this only downloads the code from the main branch of the repository
-
-# On module sources, we cannot parameterize the value of the source.
-
-# How can we tell a module source to close it from a specific branch ????
-
-
-
 module "vpc" {
   source               = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   VPC_CIDR             = var.VPC_CIDR
