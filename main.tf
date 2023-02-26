7e1077fa79915e54b3167c6c3496b54a203f94da
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:44:45 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "alb-public" {
-  source               = "./vendor/modules/alb"     // Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  source               = "./vendor/modules/alb"     
   ALB_NAME             = "roboshop-public-alb"
   ENV                  = var.ENV 
   INTERNAL             = false
@@ -7,7 +7,7 @@ module "alb-public" {
 
 
 module "alb-private" {
-  source               = "./vendor/modules/alb"     // Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  source               = "./vendor/modules/alb"
   ALB_NAME             = "roboshop-private-alb"
   ENV                  = var.ENV
   INTERNAL             = true
