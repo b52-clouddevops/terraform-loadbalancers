977e5a84181b43d46c2778f58765efc5658454d1
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:18:12 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "alb-public" {
   source               = "./vendor/modules/alb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ALB_NAME             = "roboshop-public-alb"
+  ENV                  = var.ENV 
+  
 }
 
 
