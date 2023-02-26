af18a35e0eafa4c502a167a183dc6e17e25b5a76
CodingManoj <mvp2612@hotmail.com>
2023-02-25 20:14:48 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-module "alb" {
+module "alb-public" {
   source               = "./vendor/modules/alb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
 }
 
+
+module "alb-private" {
+  source               = "./vendor/modules/alb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+}
