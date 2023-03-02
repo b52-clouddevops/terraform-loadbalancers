e2a56c6f0899f30d8c490f2fa211a8e21096bcf2
CodingManoj <mvp2612@hotmail.com>
2023-03-01 20:09:35 -0500

    Auto Push Commit

--- output.tf
+++ output.tf
@@ -11,10 +11,10 @@ output "PRIVATE_LISTENER_ARN" {
 }
 
 output "PRIVATE_ALB_ADDRESS" {
-    value =  module.alb-private.ALB_ARN
+    value =  module.alb-private.ALB_ADDRESS
 }
 
-output "PRIVATE_LISTENER_ARN" {
+output "PUBLIC_LISTENER_ARN" {
     value  = module.alb-private.LISTENER_ARN[0]
 }
 
